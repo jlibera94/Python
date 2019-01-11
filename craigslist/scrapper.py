3cf6ed2275a0bee9bcdf23e43fd7467415aa47b9
jlibera94 <jlibera94@hotmail.com>
2019-01-11 13:54:25 -0500

    added address to data

--- craigslist/scrapper.py
+++ craigslist/scrapper.py
@@ -5,9 +5,10 @@ Created on Thu Nov 15 13:18:44 2018
 @author: Jan
 """
 
-import bs4
+
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
+from csv import writer
 
 my_url = 'https://montreal.craigslist.ca/d/apts-housing-for-rent/search/apa?lang=en&cc=us'
 
@@ -23,36 +24,34 @@ page_soup = soup(page_html, "html.parser")
 containers = page_soup.findAll("li", {"class":"result-row"})
 
 filename = "apartments.csv"
-f = open(filename, "w")
+with open(filename, 'w+') as output_file:
+    csv_writer = writer(output_file, delimiter=',')
+    csv_writer.writerow(["Title", "Price", "Address"])
 
-headers = "title, price\n"
-
-f.write(headers)
 
 for container in containers:
     
     title_container = container.findAll("a", {"class":"result-title hdrlnk"})
     title = title_container[0].text
+    print(title)
     
     #**was able to print out the titles of the post currently working on 
     #printing out the prices
   
     price_container = container.findAll("span", {"class":"result-price"})
-    if price_container:
-       price = price_container[0].text
+    price = price_container[0].text
+    print(price)
     
-# =============================================================================
-#    
-#     span = container.find("span", {"class":"result-price"})
-#     if span: 
-#         container_print = span.string
-#        
-# =============================================================================
+    address_container = container.findAll("span", {"class":"result-hood"})
+    try:
+        address = address_container[0].text
+        print(address)
+    except:    
+        print("NULL")
     
-    print("title: " + title)
-    print("price: " + price)
+    with open(filename, "a", encoding='UTF-8') as output_file:
+            csv_writer = writer(output_file, delimiter=',')
+            csv_writer.writerow([title, price, address])
+
     
-    #f.write(title + "\n")
-    f.write(title + "," +  price + "\n")
-    
-f.close()    
+output_file.close()    
